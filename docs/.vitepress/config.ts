470b4ee84f5b54a7e00ceb8c94bb343fe267c5a1
Denis Bezykornov <archidevil52@gmail.com>
2023-08-02 21:54:10 +0300

    Add a new recipe + reorder

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,30 +14,31 @@ const config: UserConfig = {
       {
         text: 'Основные блюда',
         items: [
+          { text: 'Картофель в духовке с сыром и чесноком', link: '/garlic-potato.html' },
           { text: 'Курица в соусе Тикка Масала', link: '/tikka-masala.html' },
           { text: 'Курица «Кунг Пао»', link: '/kung-pao.html' },
-          { text: 'Сыр панир', link: '/paneer-cheese.html' },
-          { text: 'Картофель в духовке с сыром и чесноком', link: '/garlic-potato.html' },
-          { text: 'Паста карбонара с ветчиной и сливками', link: '/carbonara.html' },
-          { text: 'Цветная капуста, запечённая с сыром', link: '/cauliflower-with-cheese.html' },
-          { text: 'Сырный крем-суп с шампиньонами', link: '/cremma-soup.html' },
-          { text: 'Рулет из лавашей', link: '/russian-roulette.html' },
           { text: 'Лазанья', link: '/lasagna.html' },
+          { text: 'Паста карбонара с ветчиной и сливками', link: '/carbonara.html' },
           { text: 'Плов со свининой', link: '/pilaf.html' },
+          { text: 'Рулет из лавашей', link: '/russian-roulette.html' },
+          { text: 'Сыр панир', link: '/paneer-cheese.html' },
+          { text: 'Сырный крем-суп с шампиньонами', link: '/cremma-soup.html' },
+          { text: 'Цветная капуста, запечённая с сыром', link: '/cauliflower-with-cheese.html' },
         ],
       },
       {
         text: 'Десерты',
         items: [
-          { text: 'Лимонно-творожный чизкейк без выпечки', link: '/lemon-cheesecake.html' },
-          { text: 'Классические сырники', link: '/cottage-cheese-cakes.html' },
           { text: 'Ажурные блины на кефире', link: '/pancakes.html' },
+          { text: 'Классические сырники', link: '/cottage-cheese-cakes.html' },
+          { text: 'Лимонно-творожный чизкейк без выпечки', link: '/lemon-cheesecake.html' },
         ]
       },
       {
         text: 'Напитки',
         items: [
           { text: 'Глинтвейн', link: '/mulled-wine.html' },
+          { text: 'Ласси с манго', link: '/mango-lassi.html' },
         ]
       }
     ],
@@ -47,6 +48,10 @@ const config: UserConfig = {
     darkModeSwitchLabel: 'Внешний вид',
     lastUpdatedText: 'Последнее обновление',
     lastUpdated: true,
+    docFooter: {
+      prev: 'Предыдущая страница',
+      next: 'Следующая страница',
+    }
   },
 }
 
